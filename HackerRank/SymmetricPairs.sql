254cd92f5689e13d9bec84f397dfbe91ea3e9c4d
Raúl Alonso <67586098+RaAlMer@users.noreply.github.com>
2021-09-23 14:36:31 +0200

    Update SymmetricPairs.sql

--- HackerRank/SymmetricPairs.sql
+++ HackerRank/SymmetricPairs.sql
@@ -21,6 +21,6 @@ INNER JOIN functions AS f2
 ON f1.x = f2.y
     AND f1.y = f2.x
 GROUP BY f1.x, f1.y
-HAVING COUNT(f1.x) > 1 OR f1.x <= f1.y
+HAVING COUNT(f1.x) > 1 OR f1.x < f1.y
 ORDER BY f1.x
 ;
